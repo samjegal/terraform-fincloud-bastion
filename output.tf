3f212d9b07e4075355ba38b40a66b03b3e930c35
samjegal <samjegal@gmail.com>
2020-06-08 13:16:43 +0900

    ACG 그룹 ID 노출

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "서버에 할당된 공인 IP 주소"
   value       = fincloud_public_ip.bastion.address
 }
+
+output "security_group_id" {
+  description = "ACG 방화벽 그룹의 ID"
+  value       = fincloud_security_group.bastion.id
+}
